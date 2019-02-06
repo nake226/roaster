43514e94390557710fdb049b73bd0095d5fa1a74
中井貴之 <nake226@mbp.local>
2019-02-06 10:16:31 +0900

    refactor: アプリケーション設計前のソース整理

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super(props);
+
+    this.state = {
+      location: "三浦海岸"
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>こんにちは、三浦海岸🍣</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
