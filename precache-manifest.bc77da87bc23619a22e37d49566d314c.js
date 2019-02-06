ebfa8d30e3608c6c00fbc14d406031783bc3354d
中井貴之 <nake226@mbp.local>
2019-02-06 18:42:45 +0900

    Updates

---
+++ precache-manifest.bc77da87bc23619a22e37d49566d314c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b076f862e0abb5c80ad0",
+    "url": "/roaster/static/js/runtime~main.b076f862.js"
+  },
+  {
+    "revision": "45e3d7ea098bb60f663c",
+    "url": "/roaster/static/js/main.45e3d7ea.chunk.js"
+  },
+  {
+    "revision": "c695632b2c27a1704da6",
+    "url": "/roaster/static/js/1.c695632b.chunk.js"
+  },
+  {
+    "revision": "dbdff5097878e32b234b49700f2c8aa2",
+    "url": "/roaster/index.html"
+  }
+];
